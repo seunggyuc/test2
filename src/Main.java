c54060a7e1ddaa416865d493e2c092815142b169
seunggyuc <seunggyu.choi@inha.edu>
2023-04-29 09:38:33 +0900

    second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //test
+        //test22
+        //test221
+        //eeda
+    }
+}
